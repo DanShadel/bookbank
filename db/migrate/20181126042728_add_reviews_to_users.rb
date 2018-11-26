88c1db6f033da65921f39910bb0297242f06cca5
danshadel <32720919+DanShadel@users.noreply.github.com>
2018-11-26 02:15:37 -0600

    postgres doesn't like my migrations :(

--- db/migrate/20181126042728_add_reviews_to_users.rb
+++ db/migrate/20181126042728_add_reviews_to_users.rb
@@ -1,6 +1,6 @@
 class AddReviewsToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :score, :double, :default => 0
+    add_column :users, :score, :real, :default => 0
     add_column :users, :num_reviews, :integer, :default => 0
   end
 end
