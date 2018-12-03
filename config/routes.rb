ca90d93c26c8308838208efacf5e69b82c738c04
DanShadel <shadeldaniel@gmail.com>
2018-12-03 16:47:49 -0600

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :textbooks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'textbooks#index'
+  get 'pages/landing'
+  root 'pages#landing'
   post 'pages/rate'
   get 'pages/about'
   get 'pages/error'
